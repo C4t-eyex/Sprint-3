b3aecd1c89dc9a29912442cd84f730bc17464c25
Fuze7-11 <117halojhon@gmail.com>
2021-09-30 21:20:23 -0500

    Creacion JS y msql

---
+++ JS/registro.js
@@ -0,0 +1,18 @@
+let registros = []
+
+function ordenarArreglo(arreglo) {
+
+}
+
+function filtrarCorreo(arreglo) {
+
+}
+
+function agregarRegistro() {
+
+}
+
+module.exports.registros = registros;
+module.exports.filtrarCorreo = filtrarCorreo;
+module.exports.ordenarArreglo = ordenarArreglo;
+module.exports.agregarRegistro =  agregarRegistro;
