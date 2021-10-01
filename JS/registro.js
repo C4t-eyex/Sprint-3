0e04c5ac0dccefa92734386f44f90c3e7fc8f704
Fuze7-11 <117halojhon@gmail.com>
2021-09-30 22:15:14 -0500

    Modificacion

--- JS/registro.js
+++ JS/registro.js
@@ -1,17 +1,36 @@
 let registros = []
 
-function ordenarArreglo(arreglo) {
+function ordenarArreglo(a,b) {
+    registros.sort((a,b) => {
+        const apellidoA = a.apellido.toLowerCase();
+        const apellidoB = b.apellido.toLowerCase();
+        if(apellidoA < apellidoB){
+            return -1;
+        }
+        if(apellidoA > apellidoB){
+            return 1;
+        }
+        return 0;
+    });
+    console.log(registros[i]);
+};
 
-}
+function filtrarCorreo(registros){
+    let filtro = "gmail.com";
+    let filtroRegexp = RegExp(filtro);
+    resultado = registros.filter(registros => filtroRegexp.test(registro.correo) === true);
+    
+    return console.log(registros[i]);
+};
 
-function filtrarCorreo(arreglo) {
-
-}
-
-function agregarRegistro() {
-
-}
-
+function agregarRegistro(registros) {
+    for(var i=0;i<5;i++){
+        registros[i] = document.getElementsByClassName("inputs"+(i+1)).value;
+    }
+    for(i=0;i<registros.length;i++){
+        console.log(registros[i]);
+    }
+};
 module.exports.registros = registros;
 module.exports.filtrarCorreo = filtrarCorreo;
 module.exports.ordenarArreglo = ordenarArreglo;
